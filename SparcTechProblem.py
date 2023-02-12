450352eaff39d1f24ea3b026a99a0cd31d4ab7f0
J3on S3o <67287626+J3onS3o@users.noreply.github.com>
2023-02-12 22:31:44 +0000

    tried to change variable types

--- SparcTechProblem.py
+++ SparcTechProblem.py
@@ -11,22 +11,16 @@ while N <= 0:
     print("Must be a positive integer")
     N = int(input("Starting number 'N'?: "))
 
-#store player 1 and player 2's current numbers
-p1_num = N
-p2_num = 0
-#used to assign current number 
-counter = 1
-past_num = p1_num
-current_num = 0
 
-print(current_num, p1_num, p2_num, counter, past_num)
 #used to get the current number during a player's turn
+#def get_current_num(choice, counter, p1_num, p2_num):
 def get_current_num():
     #ask player2 if they want to reduce or augment
     print("Reduce: Write a positive factor of the most recently written number. \nAugment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number you most recently wrote. ")
     choice = int(input("P2: Reduce (1) or Augment (2)? "))
 
     #past_num = current_num
+   
 
     #checks to see if the int is positive
     current_num = int(input("Number? "))
@@ -50,15 +44,15 @@ def check_choice(choice):
     #Reduce: they must write a positive factor of the most recently written number.
     if choice == 1: 
         if past_num % current_num == 0:
-            print("okay")
+            print("okay") #bruh continue doesnt work lol
         else: 
             print("That number does not reduce. Try again")
             get_current_num()
 
     #Augment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number they themselves most recently wrote.
     elif choice == 1:
-        if current_num % past_num == 0:
-            print(p1_num)
+        if current_num % past_num == 0: #ALSO NEED TO CHECK THAT IT'S A POSTIIVE FACTOR
+            print(p1_num) #bruh continue doesnt work lol (same here so placeholdersss)
             print(p2_num)
         else: 
             print("That number does not augment. Try again")
@@ -67,13 +61,26 @@ def check_choice(choice):
     #updates past_num to current_num for next turn 
     past_num = current_num
 
-#code for the first turn as player 2 is forced to reduce 
-print("The first turn forces player 2 to reduce. Write a positive factor of the most recently written number. ")
-choice = 1 #make reduce "1" and augment 2
-p2_num = int(input("What is your first number? "))
-current_num = p2_num 
-print(current_num)
-counter = counter + 1
+def first_turn(): 
+     #store player 1 and player 2's current numbers
+    global p1_num
+    global p2_num
+    #used to assign current number 
+    global counter
+    global past_num
+    global current_num
+    #code for the first turn as player 2 is forced to reduce 
+    print("The first turn forces player 2 to reduce. Write a positive factor of the most recently written number. ")
+    choice = 1 #make reduce "1" and augment 2
+    p2_num = int(input("What is your first number? "))
+    current_num = p2_num 
+    print(current_num)
+    counter = 1
+    p1_num = N
+    print(choice, counter, p1_num, p2_num)
+
+#beginning of interaction 
+first_turn()
 
 while p1_num != 1 and p2_num != 1: 
     get_current_num()
