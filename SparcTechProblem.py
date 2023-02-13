e9d33cad9ba3b992b864fff781d58686c6a4d58a
J3on S3o <67287626+J3onS3o@users.noreply.github.com>
2023-02-13 14:29:49 +0000

    Some testing on checking repeating numbers.

--- SparcTechProblem.py
+++ SparcTechProblem.py
@@ -5,6 +5,16 @@ number. If they choose to augment, they must write a number that is both 1) a po
 multiple of the most recently written number, and 2) a positive factor of the number they
 themselves most recently wrote. """
 
+#make an array to store all the numbers each player writes throughout the game
+all_num = [ ]
+
+def check_repeats(current_num):
+    for item in all_num:
+        if current_num == item:
+            return False
+        else:
+            return True
+
 #used to get the current number during a player's turn
 def get_current_num(choice, past_num, p1_num, p2_num):
 
@@ -44,7 +54,6 @@ def get_current_num(choice, past_num, p1_num, p2_num):
         else: 
             print("That is not a valid player number. Try again. ")
             which_player = int(input("What player are you? (1 or 2) "))
-
     return current_num
 
 def main(): 
@@ -53,14 +62,19 @@ def main():
     while N <= 0: 
         print("Must be a positive integer")
         N = int(input("Starting number 'N'?: "))
+    all_num.append(N) 
 
     #code for the first turn as player 2 is forced to reduce 
     print("The first turn forces player 2 to reduce. Write a positive factor of the most recently written number. ")
     choice = 1 #make reduce "1" and augment 2
     p2_num = int(input("What is your first number? "))
+    if p2_num == 1:
+        print("You have submitted '1', you lose.")
+        quit()
     current_num = p2_num 
     counter = 1
     p1_num = N
+    all_num.append(p2_num) 
 
     while True:
         #ask player2 if they want to reduce or augment
@@ -68,6 +82,12 @@ def main():
         choice = int(input("Player: Reduce (1) or Augment (2)? "))
             
         current_num = get_current_num(choice, current_num, p1_num, p2_num)
+        not_repeating = check_repeats(current_num) 
+
+        while not_repeating is False: 
+            print("You can't have repeat numbers! Try your turn again!")
+            current_num = get_current_num(choice, current_num, p1_num, p2_num)
+            not_repeating = check_repeats(current_num) 
 
         #checks to see if the int is equal to 1
         if current_num == 1:
@@ -80,6 +100,9 @@ def main():
             p2_num = current_num
         else:
             p1_num = current_num
+        
+        all_num.append(current_num) 
+        print("Current numbers are " + all_num)
 
 if __name__ == "__main__":
     main()
