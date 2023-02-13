216bf8bc9eb1010a14a847b75b7e6b99e551f174
J3on S3o <67287626+J3onS3o@users.noreply.github.com>
2023-02-13 07:07:16 +0000

    Added some features to check augmented numbers

--- SparcTechProblem.py
+++ SparcTechProblem.py
@@ -19,19 +19,31 @@ def get_current_num(choice, past_num, p1_num, p2_num):
     #Reduce: they must write a positive factor of the most recently written number.
     if choice == 1: 
         if past_num % current_num == 0:
-            print("okay") #bruh continue doesnt work lol
+            print("That number reduces. Next player's turn.")
         else: 
             print("That number does not reduce. Try again")
             return get_current_num(choice, past_num, p1_num, p2_num) #Recurse until number is valid
 
     #Augment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number they themselves most recently wrote.
     elif choice == 2:
-        if current_num % past_num == 0: #ALSO NEED TO CHECK THAT IT'S A POSTIIVE FACTOR
-            print(p1_num) #bruh continue doesnt work lol (same here so placeholdersss)
-            print(p2_num)
+        which_player = int(input("What player are you? (1 or 2) "))
+        
+        #code to check if number is a positive factor of P1's most recently written number
+        if which_player == 1: 
+            if current_num % past_num == 0 and p1_num % current_num == 0: 
+                print("That number augments. Next player's turn.")
+            else: 
+                print("That number does not augment. Try again")
+                return get_current_num(choice, past_num, p1_num, p2_num)
+        elif which_player == 2:
+            if current_num % past_num == 0 and p2_num % current_num == 0: 
+                print("That number augments. Next player's turn.")
+            else: 
+                print("That number does not augment. Try again")
+                return get_current_num(choice, past_num, p1_num, p2_num)
         else: 
-            print("That number does not augment. Try again")
-            return get_current_num(choice, past_num, p1_num, p2_num)
+            print("That is not a valid player number. Try again. ")
+            which_player = int(input("What player are you? (1 or 2) "))
 
     return current_num
 
@@ -53,8 +65,8 @@ def main():
     while True:
         #ask player2 if they want to reduce or augment
         print("Reduce: Write a positive factor of the most recently written number. \nAugment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number you most recently wrote. ")
-        choice = int(input("P2: Reduce (1) or Augment (2)? "))
-
+        choice = int(input("Player: Reduce (1) or Augment (2)? "))
+            
         current_num = get_current_num(choice, current_num, p1_num, p2_num)
 
         #checks to see if the int is equal to 1
@@ -68,6 +80,6 @@ def main():
             p2_num = current_num
         else:
             p1_num = current_num
-            
+
 if __name__ == "__main__":
     main()
