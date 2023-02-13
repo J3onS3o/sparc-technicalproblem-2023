2a2cd89bfc0dbe53ff31a1050d9535ae7e234c9a
J3on S3o <67287626+J3onS3o@users.noreply.github.com>
2023-02-13 16:15:04 +0000

    Restructured the code. Checks repeat numbers

--- SparcTechProblem.py
+++ SparcTechProblem.py
@@ -1,32 +1,33 @@
-#this is a test script for a math problem just out of curiousity 
+# This is a test script for a math problem just out of curiousity 
 
 """ If a player chooses to reduce, they must write a positive factor of the most recently written
 number. If they choose to augment, they must write a number that is both 1) a positive
 multiple of the most recently written number, and 2) a positive factor of the number they
 themselves most recently wrote. """
 
-#make an array to store all the numbers each player writes throughout the game
+# Make an array to store all the numbers each player writes throughout the game
 all_num = [ ]
 
+
 def check_repeats(current_num):
-    for item in all_num:
-        if current_num == item:
-            return False
-        else:
-            return True
+    return not(current_num in all_num)
 
-#used to get the current number during a player's turn
+
+# Used to get the current number during a player's turn
 def get_current_num(choice, past_num, p1_num, p2_num):
+    while (choice != 1) and (choice != 2): 
+        print("Not a valid choice. Reduce(1) or Augment(2)")
+        choice = int(input("Player: Reduce (1) or Augment (2)? "))
 
-    #checks to see if the int is positive
+    # Checks to see if the int is positive
     current_num = int(input("Number? "))
     while current_num <= 0:
         print("Must be a positive integer")
         current_num = int(input("Number? "))
 
-    #code to check if the number is reduced or augmented properly
+    # Code to check if the number is reduced or augmented properly
 
-    #Reduce: they must write a positive factor of the most recently written number.
+    # Reduce: they must write a positive factor of the most recently written number.
     if choice == 1: 
         if past_num % current_num == 0:
             print("That number reduces. Next player's turn.")
@@ -34,11 +35,11 @@ def get_current_num(choice, past_num, p1_num, p2_num):
             print("That number does not reduce. Try again")
             return get_current_num(choice, past_num, p1_num, p2_num) #Recurse until number is valid
 
-    #Augment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number they themselves most recently wrote.
+    # Augment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number they themselves most recently wrote.
     elif choice == 2:
         which_player = int(input("What player are you? (1 or 2) "))
         
-        #code to check if number is a positive factor of P1's most recently written number
+        # Code to check if number is a positive factor of P1's most recently written number
         if which_player == 1: 
             if current_num % past_num == 0 and p1_num % current_num == 0: 
                 print("That number augments. Next player's turn.")
@@ -56,53 +57,63 @@ def get_current_num(choice, past_num, p1_num, p2_num):
             which_player = int(input("What player are you? (1 or 2) "))
     return current_num
 
+
 def main(): 
-    #get the starting integer "N"
+    # Get the starting integer "N"
     N = int(input("Starting number 'N'?: "))
     while N <= 0: 
         print("Must be a positive integer")
         N = int(input("Starting number 'N'?: "))
     all_num.append(N) 
 
-    #code for the first turn as player 2 is forced to reduce 
+    # Code for the first turn as player 2 is forced to reduce 
     print("The first turn forces player 2 to reduce. Write a positive factor of the most recently written number. ")
-    choice = 1 #make reduce "1" and augment 2
+    choice = 1
     p2_num = int(input("What is your first number? "))
+
     if p2_num == 1:
         print("You have submitted '1', you lose.")
         quit()
+
+    while N % p2_num != 0:
+        print("That number does not reduce. Try again")
+        p2_num = int(input("What is your first number? "))
+            
+
     current_num = p2_num 
     counter = 1
     p1_num = N
     all_num.append(p2_num) 
 
     while True:
-        #ask player2 if they want to reduce or augment
+        # Ask player2 if they want to reduce or augment
         print("Reduce: Write a positive factor of the most recently written number. \nAugment: write a number that is both 1) a positive multiple of the most recently written number, and 2) a positive factor of the number you most recently wrote. ")
         choice = int(input("Player: Reduce (1) or Augment (2)? "))
             
         current_num = get_current_num(choice, current_num, p1_num, p2_num)
         not_repeating = check_repeats(current_num) 
 
-        while not_repeating is False: 
+        while not_repeating == False: 
             print("You can't have repeat numbers! Try your turn again!")
             current_num = get_current_num(choice, current_num, p1_num, p2_num)
             not_repeating = check_repeats(current_num) 
 
-        #checks to see if the int is equal to 1
+        # Checks to see if the int is equal to 1
         if current_num == 1:
             print("You have submitted '1', you lose.")
             quit()
 
-        #updates the previous number placeholder
+        # Updates the previous number placeholder
         counter += 1
         if counter % 2 == 0: 
             p2_num = current_num
+            print("It's Player 2's Turn:")
         else:
             p1_num = current_num
+            print("It's Player 1's Turn: ")
         
         all_num.append(current_num) 
-        print("Current numbers are " + all_num)
+        print(all_num)
 
 if __name__ == "__main__":
     main()
